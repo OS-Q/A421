a663fb7583b7f69e5e5832867f078776482bee07
qitas <qitas@qitas.cn>
2019-06-23 20:23:28 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# sdcc-x64
-x64 sdcc bin toolchain
+﻿# [sdcc x64 bin](https://github.com/sochub/sdcc-x64)
+
+[![sites](SoC/qitas.png)](http://www.qitas.cn) 
+
+#### 归属：[SDCC](https://github.com/sochub/SDCC) 
+
+### [sdcc x64描述](https://github.com/sochub/sdcc-x64/wiki) 
+
+sdcc- x64为作为Linux x64平台编译工具
+
+###  [SoC资源平台](http://www.qitas.cn)
---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+shellPath=`pwd`
+BINPath=$HOME/SDCC
+
+function set_sdcc()
+{
+    	if [ ! -d $BINPath ]; then
+		mkdir $BINPath
+    	fi
+    	if [ ! -f $shellPath/sdcc-x64-linux.tar.bz2 ]; then
+		cd $shellPath
+		wget -O sdcc-x64-linux.tar.bz2 https://nchc.dl.sourceforge.net/project/sdcc/snapshot_builds/amd64-unknown-linux2.5/sdcc-snapshot-amd64-unknown-linux2.5-20190622-11292.tar.bz2
+    	fi
+	if [ ! -d  $BINPath/sdcc/bin ]; then
+		tar -xzvf $shellPath/sdcc-x64-linux.tar.bz2 -C $BINPath
+		echo -e "get sdcc bin \n${Line}"   	
+	else
+		echo -e "exist sdcc folder\n${Line}"
+	fi
+	if [ -d  $BINPath/sdcc/bin ]; then
+		echo 'export PATH='$BINPath'/sdcc/bin:$PATH' >> ~/.bashrc
+		source ~/.bashrc
+		echo -e "export SDCC path\n${Line}"   	
+	fi
+}
+
+set_sdcc
