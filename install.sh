a328810ff3370d1123d743e8f617a2402dafe08d
qitas <qitas@qitas.cn>
2019-06-23 20:27:56 +0800

    fix tar command

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ function set_sdcc()
 		wget -O sdcc-x64-linux.tar.bz2 https://nchc.dl.sourceforge.net/project/sdcc/snapshot_builds/amd64-unknown-linux2.5/sdcc-snapshot-amd64-unknown-linux2.5-20190622-11292.tar.bz2
     	fi
 	if [ ! -d  $BINPath/sdcc/bin ]; then
-		tar -xzvf $shellPath/sdcc-x64-linux.tar.bz2 -C $BINPath
+		tar -xf $shellPath/sdcc-x64-linux.tar.bz2 -C $BINPath
 		echo -e "get sdcc bin \n${Line}"   	
 	else
 		echo -e "exist sdcc folder\n${Line}"
